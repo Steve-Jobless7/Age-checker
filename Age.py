81150bca15821575c1080ffbdef2ba5e19690e15
MollyA246 <shaikha@taism.com>
2026-03-28 19:32:30 +0400

    This is a age checker

--- Age.py
+++ Age.py
@@ -1,6 +1,9 @@
 age = int(input("Enter your age: "))
 
-if 10 <= age <= 20:
-    print("You are between 10 and 20 years old.")
+if age >= 10:
+    if age <= 20:
+        print("You are between 10 and 20 years old.")
+    else:
+        print("You are NOT between 10 and 20 years old.")
 else:
     print("You are NOT between 10 and 20 years old.")
